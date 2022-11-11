c1438e97e7852250b41b1ffa70e069cc61ad4c42
Yuri Shornikov <dbonlyfun@gmail.com>
2022-11-11 22:17:48 +0300

    1 2 3

--- 3/count.py
+++ 3/count.py
@@ -1,41 +1,22 @@
-
-class Open_file:
-    def __init__(self, top, count_full):
-        self.top = top
-        self.count_full = count_full
-
-
 lst = ['1.txt', '2.txt', '3.txt']
+counts = {}
+dict_lst = {}
 for doc in lst:
     def open_file(doc):
         with open(doc, 'rt', encoding='utf-8') as file:
-            counts = {}
-            doc_list = {}
             r = file.read()
-            doc_list.update({doc: r})
             count = r.count('\n') + 1
-            counts.update({count: doc_list})
-            # print(counts)
-            sorted_counts = sorted(counts.items())
-            print(sorted_counts)
-            # counts = [doc]
-            # r = file.read()
-            # count = r.count('\n') + 1
-            # counts.append(count)
-            # counts.append(r)
-            # print(counts)
-
-            # print(count)
-            # print(r)
-
-
+            counts.update({doc: count})
+            dict_lst.update({doc: r})
     open_file(doc)
-# with open('1.txt', 'rt', encoding='utf-8') as file:
-#
-#     r = file.read()
-#
-#     count = r.count('\n')
-#
-#
-#     print(count)
-#     print(r)
+
+x = dict(sorted(counts.items(), key=lambda item: item[1]))
+
+for key, value in x.items():
+    for keys, values in dict_lst.items():
+        if keys == key:
+            print(keys)
+            print(counts.setdefault(keys))
+            print(values)
+
+
